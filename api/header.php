383bb071d8c18adb0b0d684b80a50082ecaed3d7
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2022-10-04 20:20:52 +0300

    Update header.php
    Bug Fix

--- api/header.php
+++ api/header.php
@@ -2,7 +2,7 @@
     function insert_head($title)
     {
         echo '<head>';
-        echo '<title>$title</title>';
+        echo '<title>' . $title . '</title>';
         echo '<meta charset="UTF-8">';
         echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
         echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">';
