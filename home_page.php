8e2e1000e556668c369c1347d3a1acb7ad3e1c06
OnurAsimIlhan <63584455+OnurAsimIlhan@users.noreply.github.com>
2022-11-29 17:19:11 +0300

    home_page template added

--- home_page.php
+++ home_page.php
@@ -1,28 +1,76 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
-<html lang="en">
-    <?php
-        include("api/header.php");
-        insert_head("Main Page");
+<html>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <body style="background-color:#EEE2E2">
         
-    ?>
-    <body>
-    <p>Please login to your account</p>
+        <!-- Header -->
+        <div style="display:flex; justify-content:space-between; padding:10px">
+            <div class="">Logo</div>
 
-    <div class="">
-        <a type="button" class="btn btn-outline-danger" href="profile.php">Profile</a>
-    </div>
+            <div style="list-style-type:none;display:flex; justify-content:flex-end; ">
+                
+                <div style="padding-right:20px">
+                    <a type="button" class="btn btn-outline-danger" href="create_event.php">Create</a>
+                </div>
+                <div style="padding-right:20px">
+                    <a type="button" class="btn btn-outline-danger" href="search_event.php">Search</a>
+                </div>
+                <div style="padding-right:10px">
+                    <a type="button" class="btn btn-outline-danger" href="profile.php">Profile</a>
+                </div>
+            </div>
 
-    <div class="">
-        <a type="button" class="btn btn-outline-danger" href="create_event.php">Create Event</a>
-    </div>
+        </div>
 
-    <div class="">
-        <a type="button" class="btn btn-outline-danger" href="search_event.php">Search Event</a>
-    </div>
 
-    <div class="">
-        <a type="button" class="btn btn-outline-danger" href="logout_process.php">Log Out</a>
-    </div>
+        <!-- Featured Events Part -->
+        <div style=" height:0px; left:20px; top:112px; border:1px solid #AE8181; background-color: #AE8181"><div>
+
+        <div style="padding:10px; margin-left:20px; margin-right:20px">
+            <h2>Featured<h2>
+            <div style="justify-content:space-between; display:flex; padding:10px">
+                <div style="height:300px; width:300px; background-color: #555">
+                    <img src="musical-note.png" alt="Event Image" width="300" height="200">
+                    <p style="font-size: 14px;margin-left:10px">Classical Music</p>
+                    <p style="font-size: 14px;margin-left:10px">Date</p>
+                    <p style="font-size: 14px;margin-left:10px">Location</p>
+                </div>
+                <div style="height:300px; width:300px; background-color: #555">
+
+                </div>
+                <div style="height:300px; width:300px; background-color: #555">
+
+                </div>
+                <div style="height:300px; width:300px; background-color: #555">
+
+                </div>
+            </div>
+        </div>
+    
+        <div style=" height:0px; left:20px; top:112px; border:1px solid #AE8181; background-color: #AE8181"><div>
+
+         <!-- All Events, List -->
+        <div style="padding:10px; margin-left:20px; margin-right:20px">
+            <div style="display:flex">
+                <h2 style="padding-right:10px">Popular Events<h2>
+                <a type="button" class="btn btn-outline-danger" href="search_event">See More</a>
+            </div>
+            
+            
+            <div style="justify-content:space-between; display:flex; padding:10px">
+                <div style="height:240px; width:180px; background-color: #555">
+                    <img src="musical-note.png" alt="Event Image" width="180" height="100">
+                    <p style="font-size: 14px; margin-left:10px">Classical Music</p>
+                    <p style="font-size: 14px; margin-left:10px">Date</p>
+                    <a type="button" class="btn btn-outline-danger" href="create_event.php" style="margin-left:10px">Create</a>
+                </div>
+
+                
+                
+                
+            </div>
+        </div>
+
     </body>
 </html>
