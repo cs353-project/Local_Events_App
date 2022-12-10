70ef188f6585ab80ba3dafc13de16d5b9df14677
Recep Uysal <recep.uysal@ug.bilkent.edu.tr>
2022-12-10 14:48:40 +0300

    search_event.php

--- search_event.php
+++ search_event.php
@@ -1,4 +1,5 @@
 <?php session_start(); ?>
+<?php include("api/connection.php"); ?>
 <!DOCTYPE html>
 <html>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -8,34 +9,34 @@
         <?php include_once("navbar.php"); ?>
         
         <div class="row" style="margin:10px">
-            <form class="col-4" style="padding-top:5px;">
+            <form class="col-4" style="padding-top:5px;" method="post" action="search_event.php" >
                 <div style="width=100px; height=100px; background-color:white; margin-top:5px">
                 <h2 style="padding:10px">Pick Categories</h2>
                     <div style="padding:10px">
-                    <input type="checkbox" class="btn-check" name="all" autocomplete="off">
+                    <input type="checkbox" class="btn-check" name="all" id="all" autocomplete="off">
                     <label class="btn btn-outline-primary" for="all">All</label>
-                    <input type="checkbox" class="btn-check" name="festival" autocomplete="off" >
+                    <input type="checkbox" class="btn-check" name="festival" id="festival" autocomplete="off" >
                     <label class="btn btn-outline-primary" for="festival">Festival</label>
                     </div>
 
                     <div style="padding:10px">
-                    <input type="checkbox" class="btn-check" id="music" autocomplete="off">
+                    <input type="checkbox" class="btn-check" name="music" id="music" autocomplete="off">
                     <label class="btn btn-outline-primary" for="music">Music</label>
-                    <input type="checkbox" class="btn-check" id="sports" autocomplete="off">
+                    <input type="checkbox" class="btn-check" name="sports id="sports" autocomplete="off">
                     <label class="btn btn-outline-primary" for="sports">Sports</label>
                     </div>
 
                     <div style="padding:10px">
-                    <input type="checkbox" class="btn-check" id="workshop" autocomplete="off">
+                    <input type="checkbox" class="btn-check" name="workshop" id="workshop" autocomplete="off">
                     <label class="btn btn-outline-primary" for="workshop">Workshop</label>
-                    <input type="checkbox" class="btn-check" id="art" autocomplete="off">
+                    <input type="checkbox" class="btn-check" name="art" id="art" autocomplete="off">
                     <label class="btn btn-outline-primary" for="art">Art</label>
                     </div>
 
                     <div style="padding:10px">
-                    <input type="checkbox" class="btn-check" id="staged" autocomplete="off">
+                    <input type="checkbox" class="btn-check" name="staged" id="staged" autocomplete="off">
                     <label class="btn btn-outline-primary" for="staged">Staged</label>
-                    <input type="checkbox" class="btn-check" id="informational" autocomplete="off">
+                    <input type="checkbox" class="btn-check" name="informational" id="informational" autocomplete="off">
                     <label class="btn btn-outline-primary" for="informational">Informational</label>
                     </div>
 
@@ -47,18 +48,17 @@
                     <div class="d-flex flex-row align-items-center mb-4" >
                                         <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline mb-0 w-20">
-                                        <input type="number" name="min_price" placeholder="min" class="form-control" required/>
+                                        <input type="number" name="min_price" placeholder="min" class="form-control"/>
                                         </div>
                         </div>
                         <div class="d-flex flex-row align-items-center mb-4" >
                                         <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline mb-0 w-20">
-                                        <input type="number" name="max_price" placeholder="max" class="form-control" required/>
+                                        <input type="number" name="max_price" placeholder="max" class="form-control"/>
                                         </div>
                         </div>
                         </div>
                 </div>
-                    
 
                 <div style="width=100px; height=100px; background-color:white; margin-top:5px">
                     <h2> Location </h2>
@@ -74,7 +74,7 @@
                     <div class="d-flex flex-row align-items-center mb-4" >
                                     <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                     <div class="form-outline mb-0 w-20">
-                                    <input type="number" name="age" placeholder="min" class="form-control" required/>
+                                    <input type="number" name="age" placeholder="age" class="form-control"/>
                                     </div>
                     </div>
                     </div>
@@ -89,6 +89,25 @@
 
             <div class="col-8">
                 <h2>Events</h2>
+
+                <?php
+                    
+                    $query = "SELECT * FROM event WHERE ";
+
+                    $result = mysqli_query($connection, $query);
+
+                    while ($row = mysqli_fetch_assoc($result))
+                    {
+                        echo '<div style="display:flex; padding:10px; justify-content:space-between;">
+                                <div style="height:300px; width:300px; background-color: #555">
+                                    <img src="musical-note.png" alt="Event Image" width="300" height="200">
+                                    <p style="font-size: 14px;margin-left:10px">Classical Music</p>
+                                    <p style="font-size: 14px;margin-left:10px">Date</p>
+                                    <p style="font-size: 14px;margin-left:10px">Location</p>
+                                </div>';
+                    }
+                ?>  
+
                 <div style="display:flex; padding:10px; justify-content:space-between;">
                     <div style="height:300px; width:300px; background-color: #555">
                         <img src="musical-note.png" alt="Event Image" width="300" height="200">
@@ -111,6 +130,7 @@
                         <p style="font-size: 14px;margin-left:10px">Location</p>
                     </div>
                 </div>
+
                 <div style="display:flex; padding:10px; justify-content:space-between;">
                     <div style="height:300px; width:300px; background-color: #555">
                         <img src="musical-note.png" alt="Event Image" width="300" height="200">
