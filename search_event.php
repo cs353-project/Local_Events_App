a92f954ce2184b03bec0f3dfa67009aacc06a53f
Recep Uysal <recep.uysal@ug.bilkent.edu.tr>
2022-12-10 14:18:10 +0300

    search event

--- search_event.php
+++ search_event.php
@@ -6,73 +6,86 @@
         
         <!-- Header -->
         <?php include_once("navbar.php"); ?>
-
-        
         
         <div class="row" style="margin:10px">
-            <div class="col-4" style="padding-top:10px;">
-                <div style="width=100px; height=100px; background-color:white; margin-top:10px">
-                    <h2 style="padding:10px">Pick Categories</h2>
+            <form class="col-4" style="padding-top:5px;">
+                <div style="width=100px; height=100px; background-color:white; margin-top:5px">
+                <h2 style="padding:10px">Pick Categories</h2>
                     <div style="padding:10px">
-                    
-                    <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btn-check-outlined">Single toggle</label>
-                    <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btn-check-outlined">Single toggle</label>
+                    <input type="checkbox" class="btn-check" name="all" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="all">All</label>
+                    <input type="checkbox" class="btn-check" name="festival" autocomplete="off" >
+                    <label class="btn btn-outline-primary" for="festival">Festival</label>
                     </div>
 
                     <div style="padding:10px">
-                    <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btn-check-outlined">Single toggle</label>
-                    <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btn-check-outlined">Single toggle</label>
+                    <input type="checkbox" class="btn-check" id="music" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="music">Music</label>
+                    <input type="checkbox" class="btn-check" id="sports" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="sports">Sports</label>
                     </div>
 
                     <div style="padding:10px">
-                    <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btn-check-outlined">Single toggle</label>
-                    <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btn-check-outlined">Single toggle</label>
+                    <input type="checkbox" class="btn-check" id="workshop" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="workshop">Workshop</label>
+                    <input type="checkbox" class="btn-check" id="art" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="art">Art</label>
                     </div>
 
                     <div style="padding:10px">
-                    <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btn-check-outlined">Single toggle</label>
-                    <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btn-check-outlined">Single toggle</label>
+                    <input type="checkbox" class="btn-check" id="staged" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="staged">Staged</label>
+                    <input type="checkbox" class="btn-check" id="informational" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="informational">Informational</label>
                     </div>
 
                 </div>
                 
-                <div style="width=100px; height=100px; background-color:white; margin-top:10px">
+                <div style="width=100px; height=100px; background-color:white; margin-top:5px">
                     <h2>Pick Price</h2>
                     <div style="display:flex">
-                    <div class="d-flex flex-row align-items-center mb-4">
+                    <div class="d-flex flex-row align-items-center mb-4" >
                                         <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline mb-0 w-20">
-                                        <input type="email" name="mail" placeholder="min" class="form-control" required/>
+                                        <input type="number" name="min_price" placeholder="min" class="form-control" required/>
                                         </div>
-                    </div>
-                    <div class="d-flex flex-row align-items-center mb-4">
+                        </div>
+                        <div class="d-flex flex-row align-items-center mb-4" >
                                         <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline mb-0 w-20">
-                                        <input type="email" name="mail" placeholder="max" class="form-control" required/>
+                                        <input type="number" name="max_price" placeholder="max" class="form-control" required/>
                                         </div>
-                    </div>
-                    </div>
-
+                        </div>
+                        </div>
                 </div>
                     
 
-                <div style="width=100px; height=100px; background-color:white; margin-top:10px">
+                <div style="width=100px; height=100px; background-color:white; margin-top:5px">
                     <h2> Location </h2>
-                    <a type="button" class="btn btn-outline-danger" href="search_event.php" style="width:100px; margin-right:10px">Near Me</a>
-                    <a type="button" class="btn btn-outline-danger" href="search_event.php" style="width:100px; margin-right:10px">In My City</a>
-                    
+                    <div style="padding:10px">
+                        <input type="radio" class="btn btn-outline-danger" name="location" value="near_me" style="width:100px; margin-right:10px" >Near Me
+                        <input type="radio" class="btn btn-outline-danger" name="location" value="in_my_city" style="width:100px; margin-right:10px" >In My City <br>
+                    </div>
                 </div>
 
-                <div style="width=100px; height=100px; background-color:white; margin-top:10px">More Parameters</div>
-            </div>
+                <div style="width=100px; height=100px; background-color:white; margin-top:5px">
+                    <h2>Age</h2>
+                    <div style="display:flex">
+                    <div class="d-flex flex-row align-items-center mb-4" >
+                                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                    <div class="form-outline mb-0 w-20">
+                                    <input type="number" name="age" placeholder="min" class="form-control" required/>
+                                    </div>
+                    </div>
+                    </div>
+                </div>
+                
+                <div style="width=100px; height=100px; background-color:white; margin-top:5px">
+                    <div class="d-flex flex-row align-items-center mb-4" >
+                        <input type="submit" class="btn btn-outline-danger" name="search_event_submit" value="Search" style="width:1000px;">
+                    </div>
+                </div>    
+            </form>
 
             <div class="col-8">
                 <h2>Events</h2>
