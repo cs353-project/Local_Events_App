4771ab348a5a112f5d45639eeb5ae33514174b9a
uygaronaterol <uygar.1212@hotmail.com>
2022-12-11 13:56:17 +0300

    create_event basic added

--- create_event.php
+++ create_event.php
@@ -3,20 +3,20 @@
 <html>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <body style="background-color:#EEE2E2">
-        
-        <!-- Header -->
-        <?php include_once("navbar.php"); ?>
+    <?php include_once("navbar.php"); ?>
+    
+     
 
-        
+        <div style=" height:0px; left:20px; top:112px; border:1px solid #AE8181; background-color: #AE8181"><div>
 
         <div style="padding:10px">
             <h2>Create Event</h2>
-            <form class="mx-1 mx-md-4" method="post" action="form_process/register_process.php">
+            <form class="mx-1 mx-md-4" method="post" action="form_process/create_event_process.php">
 
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline mb-1 w-50">
-                                            <input type="text" name="first_name" class="form-control" required/>
+                                            <input type="text" name="title" class="form-control" required/>
                                             <label class="form-label">Event Name</label>
                                         </div>
                                     </div>
@@ -24,8 +24,36 @@
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline mb-1 w-50">
-                                            <input type="text" name="middle_name" class="form-control"/>
-                                            <label class="form-label">Location</label>
+                                            <input type="text" name="address_city" class="form-control"/>
+                                            <label class="form-label">City</label>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline mb-1 w-50">
+                                            <input type="text" name="address_street" class="form-control"/>
+                                            <label class="form-label">Street</label>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline mb-1 w-50">
+                                            <input type="text" name="address_building" class="form-control"/>
+                                            <label class="form-label">Building</label>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline mb-0 w-50">
+                                        <input type="text" name="online_address" class="form-control" required/>
+                                        <label class="form-label">Online link</label>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline mb-1 w-50">
+                                            <input type="datetime-local" name="start_time" class="form-control" required/>
+                                            <label class="form-label">Start Date</label>
                                         </div>
                                     </div>
 
@@ -34,39 +62,96 @@
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline mb-1 w-50">
-                                            <input type="date" name="date" class="form-control" required/>
-                                            <label class="form-label">Your Birth Date</label>
+                                            <input type="datetime-local" name="end_time" class="form-control" required/>
+                                            <label class="form-label">End Date</label>
                                         </div>
                                     </div>
-
-                                    
 
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline mb-0 w-50">
-                                        <input type="email" name="mail" class="form-control" required/>
+                                        <input type="text" name="description" class="form-control" required/>
                                         <label class="form-label">Description</label>
                                         </div>
                                     </div>
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline mb-1 w-50">
+                                            <input type="datetime-local" name="registration_endtime" class="form-control" required/>
+                                            <label class="form-label">Registiration End Date</label>
+                                        </div>
+                                    </div>
+
+                                
 
                                     <div class="d-flex flex-row align-items-center mb-4">
                                         <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline mb-0 w-50">
-                                        <input type="password" name="password" class="form-control" required/>
-                                        <label class="form-label">Restriction</label>
+                                        <label class="form-label">Restrictions</label>
                                         </div>
                                     </div>
-
                                     <div class="d-flex flex-row align-items-center mb-4">
-                                        <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                                         <div class="form-outline mb-0 w-50">
-                                        <input type="password" name="repeat_password" class="form-control" required/>
-                                        <label class="form-label">Image</label>
+                                            <input type="checkbox" id="capacity_check" name="capacity_check[]" value="capacity_check">
+                                            <label for="capacity_check">Capacity</label><br>
+                                            <input type="checkbox" id="age_check" name="age_check[]" value="age_check">
+                                            <label for="age_check">Age</label><br>
+                                            <input type="checkbox" id="gender_check" name="gender_check[]" value="gender_check">
+                                            <label for="gender_check">Gender</label><br>
+                                            <input type="checkbox" id="pass_check" name="pass_check[]" value="pass_check">
+                                            <label for="pass_check">Pass</label><br>
                                         </div>
                                     </div>
-
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline mb-0 w-50">
+                                        <input type="number" id="capacity" name="capacity" min="0" max="100000">
+                                        <label class="form-label">Capacity Amount</label>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline mb-0 w-50">
+                                        <input type="number" id="age" name="age" min="3" max="21">
+                                        <label class="form-label">Age Restriction Over</label>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline mb-0 w-50">
+                                        <label for="gender">Gender</label>
+                                        <select id="gender" name="gender">
+                                        <option value="male">Male</option>
+                                        <option value="female">Female</option>
+                                        </select>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline mb-0 w-50">
+                                        <input type="number" id="price" name="price" min="1" max="10000">
+                                        <label class="form-label">Age Restriction Over</label>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline mb-0 w-50">
+                                        <label for="type">Event_type</label>
+                                        <select id="type" name="type">
+                                        <option value="festival">Festival</option>
+                                        <option value="music">Music</option>
+                                        <option value="sports">Sports</option>
+                                        <option value="workshop">Workshop</option>
+                                        <option value="art">Art</option>
+                                        <option value="staged">Staged</option>
+                                        </select>
+                                        </div>
+                                    </div>
                                     <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                        <a type="button" class="btn btn-outline-danger" href="register.php">Create new</a>
+                                        <input type="submit" name="create-event" value="Create Event" class="btn btn-primary btn-lg">
                                     </div>
 
                                 </form>
---
+++ form_process/create_event_process.php
@@ -0,0 +1,81 @@
+<?php
+    session_start();
+    include("../api/connection.php");
+
+    if(isset($_POST["create-event"]))
+    {   
+        $title = $_POST["title"];
+        $address_city =  $_POST["address_city"];
+        $address_street =  $_POST["address_street"];
+        $address_building =  $_POST["address_building"];
+        $online_address = $_POST["online_address"];
+        $start_time = $_POST["start_time"];
+        $end_time = $_POST["end_time"];
+        $description = $_POST["description"];
+        $registration_endtime = $_POST["registration_endtime"];
+        $current_capacity = 0;
+        $type = $_POST["type"];
+
+        
+        if(isset($_POST['capacity_check'])) {
+            $capacity = $_POST["capacity"];
+        }
+        else{
+            $capacity = null;
+        }
+        if(isset($_POST['age_check'])) {
+            $age = $_POST["age"];
+        }
+        else{
+            $age = null;
+        }
+        if(isset($_POST['gender_check'])) {
+            $gender = $_POST["gender"];
+        }
+        else{
+            $gender = null;
+        }
+        if(isset($_POST['pass_requirement'])) {
+            $pass_requirement = 't';
+        }
+        else{
+            $pass_requirement = 'i';
+        }
+
+        mysqli_real_escape_string($connection, $title);
+        mysqli_real_escape_string($connection, $address_city);
+        mysqli_real_escape_string($connection, $address_street);
+        mysqli_real_escape_string($connection, $address_building);
+        mysqli_real_escape_string($connection, $capacity);
+        mysqli_real_escape_string($connection, $description);
+        $query2 = "INSERT INTO location ( address_city, address_street, address_building, online_address) " . 
+        "VALUES ( '$address_city', '$address_street', '$address_building', '$online_address' )";
+        $result2 = mysqli_query($connection, $query2);
+        if (!$result2)
+        {
+            mysqli_error();
+        }
+        $location_id = "SELECT * FROM `location` ORDER BY location_id DESC LIMIT 1";
+        $result4 = mysqli_query($connection, $location_id);
+        while ($row = mysqli_fetch_assoc($result4))
+        {
+            $postID = $row["location_id"];
+        }
+
+        $creator_id = $_SESSION["id"];
+        $query1 = "INSERT INTO event (  location_id, title, creator_id, description, type, registration_endtime, start_time, end_time, status) " . 
+        "VALUES ('$postID', '$title', '$creator_id', '$description', '$type', '$registration_endtime', '$start_time', '$end_time','active')";
+        $result1 = mysqli_query($connection, $query1);
+        if (!$result1)
+        {
+            mysqli_error();
+        }   
+        $query3 = "INSERT INTO restriction ( event_id, capacity, age, gender, pass_requirement) " . 
+        "VALUES ( '1', '$capacity', '$age', '$gender', '$pass_requirement')";
+        $result3 = mysqli_query($connection, $query3);
+        if (!$result3)
+        {
+            mysqli_error();
+        }
+    }
+?>
