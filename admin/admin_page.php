7286d47bd2933236aa926167bd86cde1539038ce
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2022-12-18 21:30:49 +0300

    All Events Displayed

--- admin/admin_page.php
+++ admin/admin_page.php
@@ -7,7 +7,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <style>
         .wrapper{
-            width: 600px;
+            width: 1000px;
             margin: 0 auto;
         }
     </style>    
@@ -65,11 +65,67 @@
                         echo "Oops! Something went wrong. Please try again later.";
                     }
  
-                    // Close connection
-                    mysqli_close($connection);
                     ?>
                 </div>
-            </div>        
+            </div>
+            
+            
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mt-5 mb-3 clearfix">
+                        <h2 class="pull-left">Event Details</h2>
+                    </div>
+                    <?php
+                
+                    $sql = "SELECT * FROM event e " . 
+                            "JOIN location l ON e.location_id = l.location_id " . 
+                            "JOIN user u ON e.creator_id = u.id";
+                    if($result = mysqli_query($connection, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            echo '<table class="table table-bordered table-striped w-auto">';
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th>ID</th>";
+                                        echo "<th>Title</th>";
+                                        echo "<th>Creator</th>";
+                                        echo "<th>Location</th>";
+                                        // echo "<th>Type</th>";
+                                        echo "<th>Registeration End Time</th>";
+                                        echo "<th>Start Time</th>";
+                                        echo "<th>End Time</th>";
+                                        echo "<th>Current Capacity</th>";
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['event_id'] . "</td>";
+                                        echo "<td>" . $row['title'] . "</td>";
+                                        echo "<td>" . $row['first_name'] . ' ' . $row["middle_name"] . ' ' . $row["last_name"] . "</td>";
+                                        echo "<td>" . $row['address_city'] . ' ' . $row["address_street"] . ' ' . $row["address_building"] . "</td>";
+                                        echo "<td>" . $row['registration_endtime'] . "</td>";
+                                        echo "<td>" . $row['start_time'] . "</td>";
+                                        echo "<td>" . $row['end_time'] . "</td>";
+                                        echo "<td>" . $row['current_capacity'] . "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else{
+                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                        }
+                    } else{
+                        echo "Oops! Something went wrong. Please try again later.";
+                    }
+ 
+                    ?>
+                </div>
+            </div>  
+
+
+
         </div>
     </div>
 </body>
