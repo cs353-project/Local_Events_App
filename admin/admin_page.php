c0c57c59dc228e50413465e8e2322f39f4ca3dfa
Efe Erkan <72245656+EfeErkan@users.noreply.github.com>
2022-12-18 20:31:31 +0300

    Deleting users

--- admin/admin_page.php
+++ admin/admin_page.php
@@ -51,7 +51,7 @@
                                         echo "<td>";
                                             
                                             echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
-                                            echo '<a href="delete.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                            echo '<a href="delete_user.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
                                         echo "</td>";
                                     echo "</tr>";
                                 }
